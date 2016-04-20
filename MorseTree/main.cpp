6f3f8b90477c4fdcb989783188a6e5d772f36793
sabby918 <skst4b@mail.umkc.edu>
2016-04-19 20:03:27 -0500

    Update main.cpp

--- MorseTree/main.cpp
+++ MorseTree/main.cpp
@@ -1,9 +1,39 @@
 #include "MorseTree.h"
-
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
 void main() {
 	MorseTree tree;
 	tree.setUp();
+
+
+
+	string decode = "_..  __.";
+	string decode2 = "_.. . _._. ___ _.. . ._.";
+
+	string encode = "ac";
+	string encode2 = "encoder";
+
+
+	cout << setw(15) << "MORSE CODE" << endl <<
+		"====================" << endl << endl;
+
+	cout << "Decoding: " << decode << " ==> ";
+	tree.decode(decode);
+	cout << "Decoding: " << decode2 << " ==> ";
+	tree.decode(decode2);
+	cout << endl;
+
+	cout << "Encoding: " << encode << " ==> ";
+	tree.encode(encode);
+	cout << "Encoding: " << encode2 << " ==> ";
+	tree.encode(encode2);
+
+
+
+	
+
+	system("pause");
 	return;
-}
+}	
